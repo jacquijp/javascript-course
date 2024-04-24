107c76518dafd8c9985e2c6c61a25afcb678cce4
Jacqueline Jimenez Plascencia <jacquelinejp2019@gmail.com>
2024-04-24 16:40:56 +0200

    Strings.js created

---
+++ strings.js
@@ -0,0 +1,13 @@
+//Creating strings
+const firstWay = 'Single quotes'
+const secondWay = "Single quotes"
+const thirdWay = `Single quotes`
+
+//1. Concatenation: Option +
+const street = 'Fake street 99'
+const city = 'Solna'
+const address = 'My full address is: ' + street + ' ' + city
+console.log(address)
+
+
+
