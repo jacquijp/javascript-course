819ad4c6956b21cd9e07bab08e4a264c93af35ce
Jacqueline Jimenez Plascencia <jacquelinejp2019@gmail.com>
2024-04-24 14:52:55 +0200

    New file created

---
+++ mutability-immutabiliti.js
@@ -0,0 +1,18 @@
+//Primitive data type - immutable
+let number = 23
+number = number + 10
+console.log(number)
+
+let isTrue = true
+isTrue = false
+console.log(isTrue)
+
+//Complex data type - Mutable
+let user = { name: 'Peter', age: 15 }
+user.age = 20
+console.log(user)
+
+let fruits = ['apple', 'orange']
+fruits[0] = 'grape'
+console.log(fruits)
+
