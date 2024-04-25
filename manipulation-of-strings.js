6d383659aec02f6edc78477525e1cb022373a966
Jacqueline Jimenez Plascencia <jacquelinejp2019@gmail.com>
2024-04-25 17:25:25 +0200

    Examples of manipulation of strings added

--- manipulation-of-strings.js
+++ manipulation-of-strings.js
@@ -21,6 +21,21 @@ console.log(greeting.indexOf('muffin')) //searches for a word but if the word do
 console.log(greeting.lastIndexOf('o')) //last position when this character were
 console.log(greeting.slice(4,8)) //shows what exist between those positions
 
+console.log(greeting.length) //shows the length of the string
+console.log(greeting.toLocaleUpperCase()) //shows the string all in upper cases
+console.log(greeting.toLocaleLowerCase()) //shows the string all in lower cases
+
+const dividedGreeting = greeting.split(' ') //shows the string as an array with every word separated
+console.log(dividedGreeting)
+console.log(dividedGreeting[1]) //Shows the word in that position 
+
+const greetingWithSpaces = " How are you? "
+const greetingNoSpaces = greetingWithSpaces.trim() //eliminates the spaces at the begging and at the end of the string 
+console.log(greetingNoSpaces)
+
+const originalGreeting = 'Hi, World'
+const newGreeting = originalGreeting.replace('World', 'Everybody')
+console.log(newGreeting)
 
 
 
