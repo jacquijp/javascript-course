be1366e696943899040988c126aee7bfde083576
Jacqueline Jimenez Plascencia <jacquelinejp2019@gmail.com>
2024-04-29 20:33:19 +0200

    numbers.js created

---
+++ numbers.js
@@ -0,0 +1,35 @@
+//1. Integer and decimal number
+const entero = 42
+const decimal = 3.14
+console.log(typeof entero, typeof decimal)
+
+//2. Scientific notation
+const scientific = 5e3
+
+//3. Infinities and NaN
+const inifinito = Infinity
+const notAnumber = NaN
+
+//Arithmetic operations
+const sum = 3 + 4
+const rest = 4 - 4
+const multiplication = 4 * 7
+const division = 16 / 2
+
+//Modules and Exponents 
+const modulo = 15 % 8
+const exponente = 2 ** 3
+
+//Precision
+const result = 0.1 + 0.2
+console.log(result)
+console.log(result.toFixed(1))
+console.log(result === 0.3)
+
+//Advanced operations
+const squareRoot = Math.sqrt(16)
+const absoluteValue = Math.abs(-7)
+const random = Math.random()
+console.log(squareRoot)
+console.log(absoluteValue)
+console.log(random)
