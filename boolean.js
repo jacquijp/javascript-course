3fe2136720d80ad017b8dc94d5d64f95b8a15671
Jacqueline Jimenez Plascencia <jacquelinejp2019@gmail.com>
2024-04-29 20:44:25 +0200

    boolean.js created

---
+++ boolean.js
@@ -0,0 +1,18 @@
+
+const isActive = true
+const hasPermission = false
+
+
+//Implicit conversion
+const result = 5 > 3
+console.log(result)
+
+const name = 'Platzi'
+console.log(!!name)
+
+//Explicit conversion
+const value = 0
+const otherValue = -24
+const explicitBoolean = Boolean(otherValue)
+console.log(explicitBoolean)
+
